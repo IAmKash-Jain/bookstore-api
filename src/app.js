96e9aa402cadd4557245aabc521636b3d55231f3
Kashish Jain <yashjn39@gmail.com>
2022-06-27 11:15:39 +0530

    Cors Added

--- src/app.js
+++ src/app.js
@@ -2,11 +2,13 @@
 require("../src/db/conn");
 const express = require("express");
 const storeRoute = require("../src/routers/storeRoute");
+const cors = require("cors");
 //End Section: Require
 
 const port = process.env.PORT || 8000;
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(storeRoute);
 
